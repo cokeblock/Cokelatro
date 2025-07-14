b07d4e4f472be6b7a78f9fde9b87099fc9a83a7c
cokeblock <108153379+cokeblock@users.noreply.github.com>
2025-07-13 21:17:46 -0400

    Add files via upload

---
+++ jokers/exponentia.lua
@@ -0,0 +1,64 @@
+SMODS.Joker{ --Referencia
+    name = "Referencia",
+    key = "exponentia",
+    config = {
+        extra = {
+            reference = 1
+        }
+    },
+    loc_txt = {
+        ['name'] = 'Referencia',
+        ['text'] = {
+            [1] = 'Gains {C:dark_edition}^0.03 mult{} when a card with an',
+            [2] = '{C:edition}Edition {}or an {C:attention}Enhancement{} is scored',
+            [3] = '{C:inactive}(currently {}{C:dark_edition}^#1#{} {C:inactive}mult){}'
+        }
+    },
+    pos = {
+        x = 7,
+        y = 6
+    },
+    cost = 300,
+    rarity = "cokelatr_ascend",
+    blueprint_compat = true,
+    eternal_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'CustomJokers',
+
+    in_pool = function(self, args)
+        return false
+    end,
+
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.extra.reference}}
+    end,
+
+    set_ability = function(self, card, initial)
+        card:add_sticker('rental', true)
+    end,
+
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play and not context.blueprint then
+            if ((function()
+        local enhancements = SMODS.get_enhancements(context.other_card)
+        for k, v in pairs(enhancements) do
+            if v then
+                return true
+            end
+        end
+        return false
+    end)() or context.other_card.edition ~= nil) then
+                card.ability.extra.reference = (card.ability.extra.reference) + 0.03
+                return {
+                    message = "UPGRADE!"
+                }
+            end
+        end
+        if context.cardarea == G.jokers and context.joker_main then
+                return {
+                    e_mult = card.ability.extra.reference
+                }
+        end
+    end
+}
---
+++ jokers/theradiodemon.lua
@@ -0,0 +1,124 @@
+SMODS.Joker{ --The Radio Demon
+    name = "The Radio Demon",
+    key = "theradiodemon",
+    config = {
+        extra = {
+            Alas = 1,
+            Xmult = 3
+        }
+    },
+    loc_txt = {
+        ['name'] = 'The Radio Demon',
+        ['text'] = {
+            [1] = '{X:red,C:white}X3{} Mult per scored',
+            [2] = '{C:attention}card{} if there are {X:attention,C:white}0{}',
+            [3] = '{E:1}technology themed{} {C:attention}jokers{} owned'
+        }
+    },
+    pos = {
+        x = 1,
+        y = 6
+    },
+    cost = 15,
+    rarity = "cokelatr_incredulous",
+    blueprint_compat = true,
+    eternal_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'CustomJokers',
+    soul_pos = {
+        x = 2,
+        y = 6
+    },
+
+    loc_vars = function(self, info_queue, card)
+        return {vars = {}}
+    end,
+
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play and not context.blueprint then
+            if ((function()
+      for i = 1, #G.jokers.cards do
+          if G.jokers.cards[i].config.center.key == "j_cokelatr_anttenna" then
+              return false
+          end
+      end
+      return true
+  end)() and (function()
+      for i = 1, #G.jokers.cards do
+          if G.jokers.cards[i].config.center.key == "j_cokelatr_tvtime" then
+              return false
+          end
+      end
+      return true
+  end)() and (function()
+      for i = 1, #G.jokers.cards do
+          if G.jokers.cards[i].config.center.key == "j_cokelatr_exe" then
+              return false
+          end
+      end
+      return true
+  end)() and (function()
+      for i = 1, #G.jokers.cards do
+          if G.jokers.cards[i].config.center.key == "j_photograph" then
+              return false
+          end
+      end
+      return true
+  end)() and (function()
+      for i = 1, #G.jokers.cards do
+          if G.jokers.cards[i].config.center.key == "j_scam" then
+              return false
+          end
+      end
+      return true
+  end)() and (function()
+      for i = 1, #G.jokers.cards do
+          if G.jokers.cards[i].config.center.key == "j_cokelatr_crt" then
+              return false
+          end
+      end
+      return true
+  end)() and (function()
+      for i = 1, #G.jokers.cards do
+          if G.jokers.cards[i].config.center.key == "j_cokelatr_em" then
+              return false
+          end
+      end
+      return true
+  end)() and (function()
+      for i = 1, #G.jokers.cards do
+          if G.jokers.cards[i].config.center.key == "j_cokelatr_garry" then
+              return false
+          end
+      end
+      return true
+  end)() and (function()
+      for i = 1, #G.jokers.cards do
+          if G.jokers.cards[i].config.center.key == "j_cokelatr_jj" then
+              return false
+          end
+      end
+      return true
+  end)() and (function()
+      for i = 1, #G.jokers.cards do
+          if G.jokers.cards[i].config.center.key == "j_cokelatr_v1" then
+              return false
+          end
+      end
+      return true
+  end)() and (function()
+      for i = 1, #G.jokers.cards do
+          if G.jokers.cards[i].config.center.key == "j_cokelatr_jkr" then
+              return false
+          end
+      end
+      return true
+  end)()) then
+                return {
+                    Xmult = card.ability.extra.Xmult
+                }
+            end
+        end
+    end
+}
